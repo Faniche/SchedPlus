f5cd24cb905f92b2978cb72f3ccaaaa421431321
faniche <guoyanan23215@gmail.com>
2022-03-15 20:37:57 +0800

    VLAN PCP definition

---
+++ src/lib/components/MyVlan.h
@@ -0,0 +1,23 @@
+//
+// Created by faniche on 2022/3/15.
+//
+
+#ifndef SCHEDPLUS_MYVLAN_H
+#define SCHEDPLUS_MYVLAN_H
+
+
+/* Definition of Priority Code Point for vlan frame
+ * https://en.wikipedia.org/wiki/IEEE_P802.1p
+ **/
+enum PRIORITY_CODE_POINT {
+    BK,     // 0 (lowest)       Background
+    BE,     // 1 (default)      Best effort
+    EE,     // 2                Excellent effort
+    CA,     // 3                Critical applications
+    VI,     // 4                Video, < 100 ms latency and jitter
+    VO,     // 5                Voice, < 10 ms latency and jitter
+    IC,     // 6                Internetwork control
+    NC      // 7 (highest)      Network control
+};
+
+#endif //SCHEDPLUS_MYVLAN_H
