6cf07183f980d6e2af7d27606cc0f6d2de056154
faniche <guoyanan23215@gmail.com>
2022-04-19 09:05:35 +0800

    refactor some code of random

--- src/Utils.h
+++ src/Utils.h
@@ -8,8 +8,8 @@
 // Created by faniche on 2021/11/27.
 //
 
-#ifndef Z3_SMT_UTILS_H
-#define Z3_SMT_UTILS_H
+#ifndef SCHEDPLUS_UTILS_H
+#define SCHEDPLUS_UTILS_H
 
 #include <random>
 #include <spdlog/spdlog.h>
@@ -17,76 +17,73 @@
 #include "components/Flow.h"
 #include "Route.h"
 
-//#define MIN_FRAME_LEN 64
-//
-//#define MAX_FRAME_LEN 1000
-//
-//#define MIN_FRAME_PERIOD 1
-//
-//#define MAX_FRAME_PERIOD 200
+class Util {
+private:
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> randInteger;
 
-constexpr static int MIN_FRAME_LEN = 64;
-constexpr static int MAX_FRAME_LEN = 1000;
-constexpr static int MIN_FRAME_PERIOD = 0;
-constexpr static int MAX_FRAME_PERIOD = 7;
-
-
+public:
 /* https://stackoverflow.com/questions/3154454/what-is-the-most-efficient-way-to-calculate-the-least-common-multiple-of-two-int */
-static long long gcd(long long int a, long long int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
-}
-
-static long long lcm(long long a, long long b) {
-    if (a > b)
-        return (a / gcd(a, b)) * b;
-    else
-        return (b / gcd(a, b)) * a;
-}
-
-long long getHyperPeriod(std::vector<Flow> flows) {
-    long long hyperPeriod, tmp = flows[0].getPeriod();
-    for (size_t i = 1; i < flows.size(); ++i) {
-        hyperPeriod = lcm(tmp, flows[i].getPeriod());
-        tmp = hyperPeriod;
+    static long long gcd(long long int a, long long int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
     }
-    return hyperPeriod;
-}
 
-static int genRandInRange(int invokeIdx) {
-    static std::random_device randomDevice;
-    static std::default_random_engine engine(randomDevice());
-
-    switch (invokeIdx) {
-        case 0:
-            static std::uniform_int_distribution<int> randPeriod(MIN_FRAME_PERIOD, MAX_FRAME_PERIOD);
-            return randPeriod(engine);
-        case 1:
-            static std::uniform_int_distribution<int> randFrameLen(MIN_FRAME_LEN, MAX_FRAME_LEN);
-            return randFrameLen(engine);
-        default:
-            return 0;
+    static long long lcm(long long a, long long b) {
+        if (a > b)
+            return (a / gcd(a, b)) * b;
+        else
+            return (b / gcd(a, b)) * a;
     }
-}
 
-int genRandFramePeriod() {
-    int ret = (int) std::pow(2, genRandInRange(0));
-
-    return ret;
-}
-
-int genRandFrameLen() {
-    return genRandInRange(1);
-}
-
-int getWholeSendInterval(const std::vector<Flow> &flows) {
-    int ret = 0;
-    for (auto &flow: flows) {
-        ret += flow.getFrameLength() * 24;
+    /**
+     * @brief unit: μs
+     */
+    static uint64_t getHyperPeriod(std::vector<Flow> &flows, std::ostringstream &oss) {
+        oss.str("");
+        long long hyperPeriod = 0, a = 0, b = 0;
+        for (size_t i = 0; i < flows.size(); ++i) {
+            if (flows[i].getPeriod() != 0) {
+                if (a == 0) {
+                    oss << "period: {" << flows[i].getPeriod();
+                    a = flows[i].getPeriod() / 100;
+                    continue;
+                }
+                b = flows[i].getPeriod() / 100;
+                oss << ", " << flows[i].getPeriod();
+                hyperPeriod =lcm(a, b);
+                a = hyperPeriod;
+            }
+        }
+        oss << "}";
+        hyperPeriod *= 100;
+        for (auto &flow: flows) {
+            flow.setHyperperiod(hyperPeriod);
+        }
+        spdlog::set_level(spdlog::level::debug);
+        spdlog::debug("Periods(unit: μs) of flows: {}, hyperperiod: {}μs", oss.str(), hyperPeriod);
+        return hyperPeriod;
     }
-    return ret;
-}
 
+    int getRandInt(size_t min, size_t max) {
+        std::uniform_int_distribution<int>::param_type param(min, max);
+        randInteger.param(param);
+        return randInteger(generator);
+    }
 
-#endif //Z3_SMT_UTILS_H
+    PRIORITY_CODE_POINT getRandPCP() {
+//        return getRandInt(0, 7);
+        return getRandInt(5, 6);
+    }
+
+    int getRandESIdx(std::vector<Node *> &esList) {
+        return getRandInt(0, esList.size() - 1);
+    }
+
+    int getDDL(PRIORITY_CODE_POINT priority) {
+        return 0;
+    }
+};
+
+#endif //SCHEDPLUS_UTILS_H
